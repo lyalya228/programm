49760a7e3b2638a7d2360c9b2094952cbdacee18
lyalya228 <156933006+lyalya228@users.noreply.github.com>
2024-01-17 19:29:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def check_psw(a):
+    psw = 'dfghfghg'
+    if a == psw:
+        print("True")
+    else:
+        print('False')
+
+check_psw("fdkjbsdkljfgbsdbndfsndnsavbhjlsabvsjhcbjlsndbclskvb;ksadvnbvvfb")
