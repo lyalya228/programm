d041cd660d7307abcb7a50ca935ad7428ab92d82
lyalya228 <156933006+lyalya228@users.noreply.github.com>
2024-05-08 19:48:55 +0300

    project_2

--- draniki.py
+++ draniki.py
@@ -8,10 +8,11 @@ win = pg.display.set_mode(size)
 fon = pg.image.load('fon.png')
 fon = pg.transform.scale(fon, (600, 500))
 
-
 meteor = pg.image.load('bolshoi_meteorit.png')
 
 meteor = pg.transform.scale(meteor, (700, 700))
+
+fond = pg.image.load('fond.png')
 
 yoda = pg.image.load('yoda_v_bochke.png')
 
@@ -23,6 +24,11 @@ samolet = pg.transform.scale(samolet, (500, 500))
 
 y = 250
 x = 250
+x_meteor = 600
+x_samolet = 1000
+y2 = 450
+x2 = 450
+
 
 while True:
     for event in pg.event.get():
@@ -39,9 +45,24 @@ while True:
 
     win.blit(fon, (0, 0))
     win.blit(meteor, (750, 352))
-    if meteor.
+
+    x_meteor -= 2
+    x_samolet -= 2
+
+    if x_meteor < -450:
+        x_meteor = 600
+
+    if x_samolet < -450:
+        x_samolet = 600
+
+    if x2 < yoda.get_rect() < x2 + 675 and y2 < yoda < y2 + 675:
+        win.blit(yoda, (10000, 10000))
+        win.blit(samolet, (10000, 10000))
+        win.blit(meteor, (10000, 1000))
+        win.blit(fond, (500, 500))
 
     win.blit(yoda, (0, y))
-    win.blit(samolet, (675, 437))
-    pg.time.delay(10)
+    win.blit(samolet, (x_samolet, 20))
+    win.blit(meteor, (x_meteor, 100))
+    pg.time.delay(100)
     pg.display.update()
